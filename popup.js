4f2a93c1afb9ce31812dcf2204a943fbc1da64c6
manasprasad7 <73057590+manasprasad7@users.noreply.github.com>
2021-08-05 20:55:16 +0530

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,10 @@
+document.querySelector(".open").addEventListener("click", function(){
+  document.querySelector(".popup").style.display = "none";
+})
+
+document.querySelector(".close").addEventListener("click", function(){
+  
+    window.close()
+  
+})
+
